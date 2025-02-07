44799bf7df10e2ef84a0b92bd63c7ecef72ad2f4
WilsonC002 sorry but i'm number 1 <113398@emmauscollege.nl>
2025-02-07 10:44:40 +0000

    very very cheap 1 pound fish

--- db/create.sql
+++ db/create.sql
@@ -20,10 +20,12 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Fish 1', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Fish 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Fish Zekeriya', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Fish 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Fish 5, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Fish 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Kabeljauw', 'Very very cheap 1 pound fish', '816905633-0', 10);
+insert into products (name, description, code, price) values ('Forel', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 20);
+insert into products (name, description, code, price) values ('Fish Zekeriya', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 50);
+insert into products (name, description, code, price) values ('Sliptong', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 30);
+insert into products (name, description, code, price) values ('Dorade', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 40);
+insert into products (name, description, code, price) values ('Zalm', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 60);
+insert into products (name, description, code, price) values ('BlueFish', 'Very very cheap 1 pound fish', '346814286-8', 70);
+insert into products (name, description, code, price) values ('Koi Fish', 'Very very good 1 pound fish', '013578479-0', 100);
 
