b02e7e5abafa70e32f3762d20ead507d72900796
WilsonC002 sorry but i'm number 1 <113398@emmauscollege.nl>
2025-02-10 11:30:53 +0000

    ni

--- db/create.sql
+++ db/create.sql
@@ -2,6 +2,23 @@
 -- create tables
 --
 
+CREATE TABLE WaterType(
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255),
+  description TEXT,
+
+
+
+)
+
+CREATE TABLE categories(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255),
+  description TEXT,
+
+
+)
+
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
@@ -28,4 +45,11 @@ insert into products (name, description, code, price) values ('Dorade', 'Nulla u
 insert into products (name, description, code, price) values ('Zalm', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 60);
 insert into products (name, description, code, price) values ('BlueFish', 'Very very cheap 1 pound fish', '346814286-8', 70);
 insert into products (name, description, code, price) values ('Koi Fish', 'Very very good 1 pound fish', '013578479-0', 100);
+
+insert into catagories(name, description) values('Fish', 'Fish');
+insert into catagories(name, description) values('ShellFish', 'ShellFish');
+insert into catagories(name, description) values('Exotic Fish', 'exotic Fish');
+insert into catagories(name, description) values('Pet Fish', 'Pet Fish');
+
+
 
