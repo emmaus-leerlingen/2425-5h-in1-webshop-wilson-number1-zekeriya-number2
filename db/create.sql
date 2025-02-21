98d2e148b4e8bd83ab717ef33822557200840add
WilsonC002 sorry but i'm number 1 <113398@emmauscollege.nl>
2025-02-21 10:22:52 +0000

    go jover

--- db/create.sql
+++ db/create.sql
@@ -35,14 +35,18 @@ insert into products (name, description, code, price, category_id) values ('Dora
 insert into products (name, description, code, price, category_id) values ('Zalm', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 60, 1);
 insert into products (name, description, code, price, category_id) values ('BlueFish', 'Very very cheap 1 pound fish', '346814286-8', 70, 1);
 insert into products (name, description, code, price, category_id) values ('Koi Fish', 'Very very good 1 pound fish', '013578479-0', 100, 4);
-insert into products (name, description, code, price, category_id) values ('Crab', 'Very very good 1 pound crab', '293829104-4', 100, 2);
-insert into products (name, description, code, price, category_id) values ('mussel', 'Very very good 1 pound mussel', '820451759-9', 100, 2);
-insert into products (name, description, code, price, category_id) values ('WhaleShark', 'Very very good 1000 pound WhaleShark', '239471047-6', 100, 3);
+insert into products (name, description, code, price, category_id) values ('Crab', 'Very very good 1 pound crab', '293829104-4', 50, 2);
+insert into products (name, description, code, price, category_id) values ('mussel', 'Very very good 1 pound mussel', '820451759-9', 10, 2);
+insert into products (name, description, code, price, category_id) values ('WhaleShark', 'Very very good 1000 pound WhaleShark', '239471047-6', 10000, 3);
 insert into products (name, description, code, price, category_id) values ('Lionfish', 'Very very good 1 pound lionfish', '930284917-3', 100, 3);
-insert into products (name, description, code, price, category_id) values ('Devils Hole PupFish', 'Very very good 1 pound devvils hole pupfish', '7283928193-2', 100, 3);
-insert into products (name, description, code, price, category_id) values ('BlueFin tuna', 'Very very good 500 pound BlueFin tuna', '283049123-8', 100, 3);
-insert into products (name, description, code, price, category_id) values ('Lobster', 'Very very good 1 pound Lobster', '819238471-2', 100, 2);
-insert into products (name, description, code, price, category_id) values ('Oyster', 'Very very good 1 pound Oyster', '728394012-2', 100, 2);
+insert into products (name, description, code, price, category_id) values ('Devils Hole PupFish', 'Very very good 1 pound devils hole pupfish', '7283928193-2', 200, 3);
+insert into products (name, description, code, price, category_id) values ('BlueFin tuna', 'Very very good 500 pound BlueFin tuna', '283049123-8', 150, 3);
+insert into products (name, description, code, price, category_id) values ('Lobster', 'Very very good 1 pound Lobster', '819238471-2', 15, 2);
+insert into products (name, description, code, price, category_id) values ('Oyster', 'Very very good 1 pound Oyster', '728394012-2', 20, 2);
+insert into products (name, description, code, price, category_id) values ('The Betta', 'Very very good 1 pound The Betta', '829374213-4', 60, 4);
+insert into products (name, description, code, price, category_id) values ('Gold Fish', 'Very very good 1 pound Gold fish', '639201923-4', 30, 4);
+insert into products (name, description, code, price, category_id) values ('Guppies', 'Very very good 1 pound Guppies', '718293847-4', 60, 4);
+
 
 insert into categories(name, description) values('Fish', 'Fish');
 insert into categories(name, description) values('ShellFish', 'ShellFish');
