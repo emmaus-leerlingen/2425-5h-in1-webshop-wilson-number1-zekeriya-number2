f3014d5b1a79775e2b168147d8c8533a7c7b2211
ZekeriyaH H <113447@emmauscollege.nl>
2025-02-23 15:33:36 +0000

    cwazy work

--- db/create.sql
+++ db/create.sql
@@ -7,7 +7,7 @@ CREATE TABLE categories (
 
 CREATE TABLE colours (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(255)
+  name VARCHAR(50)
 );
 
 CREATE TABLE products (
@@ -25,19 +25,18 @@ CREATE TABLE products (
 CREATE TABLE products_colours (
     products_id INT,
     colours_id INT,
-    FOREIGN KEY (product_id) REFERENCES product(product_id),
-    FOREIGN KEY (kleur_id) REFERENCES kleur(kleur_id),
-    PRIMARY KEY (product_id, kleur_id)
+    FOREIGN KEY (products_id) REFERENCES products(id),
+    FOREIGN KEY (colours_id) REFERENCES colours(id),
+    PRIMARY KEY (products_id, colours_id)
 );
 
 CREATE TABLE customers (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  FirstName VARCHAR(255),
-  LastName VARCHAR(255),
+  FirstName VARCHAR(100),
+  LastName VARCHAR(100),
   Email VARCHAR(255),
-  PhoneNumber VARCHAR(255),
-  Addresses VARCHAR(255),
-  description TEXT
+  PhoneNumber VARCHAR(20),
+  Addresses TEXT
 );
 
 CREATE TABLE reviews (
@@ -45,7 +44,7 @@ CREATE TABLE reviews (
   products_id INTEGER,
   customers_id INTEGER,
   comment_text TEXT,
-  rating NUMERIC(2,1),
+  rating NUMERIC(3,1),
   FOREIGN KEY (products_id) REFERENCES products(id),
   FOREIGN KEY (customers_id) REFERENCES customers(id)
 );
@@ -86,9 +85,9 @@ insert into products (name, description, code, price, categories_id, colours_id)
 insert into products (name, description, code, price, categories_id, colours_id) values ('Guppies', 'Very very good 1 pound Guppies', '718293847-4', 60, 4, 7);
 insert into products (name, description, code, price, categories_id, colours_id) values ('test', 'Very very cheap 1 pound test', '234146738-0', 10, 1, 1);
 insert into products (name, description, code, price, categories_id, colours_id) values ('2', 'Very very cheap 1 pound 2', '634357259-0', 10, 1, 1);
-insert into products (name, description, code, price, categories_id, colours_id) values ('exotisch1', 'Very very good 1000 pound WhaleShark', '532465785-7', 10000, 3, 6);
-insert into products (name, description, code, price, categories_id, colours_id) values ('exotisch2', 'Very very good 1000 pound WhaleShark', '231546756-0', 10000, 3, 6);
-insert into products (name, description, code, price, categories_id, colours_id) values ('exotisch 3', 'Very very good 1000 pound WhaleShark', '233409865-6', 10000, 3, 6);
+insert into products (name, description, code, price, categories_id, colours_id) values ('Barrango', 'Very very good 1 pound Barrango', '532465785-7', 10000, 3, 6);
+insert into products (name, description, code, price, categories_id, colours_id) values ('Angelfish', 'Very very good 1000 pound Angelfish', '231546756-0', 10000, 3, 6);
+insert into products (name, description, code, price, categories_id, colours_id) values ('Arowana', 'Very very good 1000 pound Arowana', '233409865-6', 10000, 3, 6);
 
 insert into customers (FirstName, LastName, Email, PhoneNumber, Addresses) values ('Elizabeth', 'Smith', 'elizasmith32@gmail.com', '0648362973', 'Tulpweg 44, Rotterdam');
 insert into customers (FirstName, LastName, Email, PhoneNumber, Addresses) values ('James', 'Williams', 'willj77@hotmail.com', '0617582374', 'Schilderswijk 86, Den Haag');
